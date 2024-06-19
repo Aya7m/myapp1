0d83efcd623ef8ce11e178549ac61f3a4d973801
Aya7m <yyoyo6987@gmail.com>
2024-06-19 18:29:57 +0300

    kkk

--- middleware/checkEmailExist.js
+++ middleware/checkEmailExist.js
@@ -9,7 +9,7 @@ let conn = connection
 export const checkEmailExist = (req, res, next) => {
 
     conn.execute(`select email from users where email='${req.body.email}'`, (err, data) => {
-        if (data.length != 0) return res.status(409).json({ message: 'account already exist' })
+        if (data?.length != 0) return res.status(409).json({ message: 'account already exist' })
             req.body.password=bycrpt.hashSync(req.body.password,8)
 
         next()
